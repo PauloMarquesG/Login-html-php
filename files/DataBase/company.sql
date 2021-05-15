439c4878e962d1af47b6caf708f49e3c267e2fe6
Paulo Marques Gonçalves <82685447+PauloMarquesG@users.noreply.github.com>
2021-05-14 21:41:30 -0300

    Update company.sql

--- files/DataBase/company.sql
+++ files/DataBase/company.sql
@@ -2,7 +2,6 @@
 -- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
 -- Generation Time: May 15, 2021 at 12:40 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
@@ -11,22 +10,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `company`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
@@ -38,7 +21,3 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`idUser`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
